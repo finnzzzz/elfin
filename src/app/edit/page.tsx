93dc1af278170222d0ada81d3235816456c8f79f
finnzzzz <o8786791@gmail.com>
2023-06-22 17:58:07 +0800

    feat(edit): set up basic react-flow

---
+++ src/app/edit/page.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import ReactFlow from 'reactflow';
+
+import 'reactflow/dist/style.css';
+
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+
+const edit = () => {
+  return (
+    <div className='h-full w-full'>
+      <ReactFlow nodes={initialNodes} edges={initialEdges} />
+    </div>
+  );
+};
+
+export default edit;
