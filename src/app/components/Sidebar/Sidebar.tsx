56acdc4401e6d2d3c93e204b6c1508bf74f289af
finnzzzz <o8786791@gmail.com>
2023-06-26 15:56:43 +0800

    feat: add basic google login functionality

--- src/app/components/Sidebar/Sidebar.tsx
+++ src/app/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,40 @@
+'use client';
+
+import Link from 'next/link';
+import { auth, authProvider } from '@/app/lib/firebase';
+import { signInWithPopup, signOut } from 'firebase/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
 const Sidebar = () => {
+  const [user, loading] = useAuthState(auth);
+
+  const login = async () => {
+    await signInWithPopup(auth, authProvider);
+  };
+
+  const logout = async () => {
+    await signOut(auth);
+  };
   return (
-    <div className='min-h-full w-12 border border-blue-900 bg-blue-100'>
-      bar
+    <div className='flex h-full w-16 flex-col items-center gap-2 border border-blue-900 bg-blue-100'>
+      <div>
+        <Link href='/'>home</Link>
+      </div>
+      {user ? (
+        <>
+          <span>{user.displayName}</span>
+          <button
+            onClick={logout}
+            className=' w-fit rounded-sm border border-blue-700 p-1'
+          >
+            logout
+          </button>
+        </>
+      ) : (
+        <button onClick={login} className=' w-fit rounded-sm border border-blue-700 p-1'>
+          login
+        </button>
+      )}
     </div>
   );
 };
