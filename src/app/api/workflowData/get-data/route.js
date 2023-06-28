adafa8ef71cbabe4d0259b3515cfcadccc542bca
finnzzzz <o8786791@gmail.com>
2023-06-28 18:43:53 +0800

    feat: add get firestore API

---
+++ src/app/api/workflowData/get-data/route.js
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '@/app/lib/firebase';
+
+export async function GET() {
+  const allData = [];
+  const res = await getDocs(collection(db, 'workflowtest'));
+  res.forEach((doc) => {
+    allData.push(doc.data());
+  });
+  console.log(allData);
+
+  return NextResponse.json(allData);
+}
