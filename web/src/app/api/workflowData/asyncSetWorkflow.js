d8413c331997b5e8488b4c771e8b5a285b2ece9f
finnzzzz <o8786791@gmail.com>
2023-07-01 15:00:28 +0800

    feat: correct setWorkflow function

--- web/src/app/api/workflowData/asyncSetWorkflow.js
+++ web/src/app/api/workflowData/asyncSetWorkflow.js
@@ -1,14 +1,17 @@
-import { setDoc, doc, serverTimestamp } from 'firebase/firestore';
-import { workflowCollectionDocRef } from '@/app/lib/firebase';
+import { setDoc, addDoc, doc, serverTimestamp, collection } from 'firebase/firestore';
+import { db } from '@/app/lib/firebase';
 
-const asyncSetWorkflow = async ({ flow }) => {
-  const workflowDoc = doc(workflowCollectionDocRef);
-  const docRef = await setDoc(workflowDoc, {
-    id: workflowDoc.id,
+const asyncSetWorkflow = async (uid, { flow }) => {
+  const workflowDoc = collection(db, 'users', uid, 'scripts');
+  const docRef = await addDoc(workflowDoc, {
+    id: '',
     name: 'none',
     flow,
     saveTime: serverTimestamp(),
   });
+
+  const docId = docRef.id;
+  await setDoc(doc(db, 'users', uid, 'scripts', docId), { id: docId }, { merge: true });
   return docRef;
 };
 
