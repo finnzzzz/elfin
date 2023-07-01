b1d55caaf95f2ba727a43dbbd057641f27b6d722
finnzzzz <o8786791@gmail.com>
2023-07-01 14:59:56 +0800

    feat: correct deleteWorkflow function

--- web/src/app/api/workflowData/asyncDeleteWorkflow.js
+++ web/src/app/api/workflowData/asyncDeleteWorkflow.js
@@ -1,9 +1,8 @@
-import { deleteDoc } from 'firebase/firestore';
-import getDocRef from './utils/getDocRef';
+import { deleteDoc, doc } from 'firebase/firestore';
+import { db } from '@/app/lib/firebase';
 
-const asyncDeleteWorkflow = async (id = '') => {
-  //    await deleteDoc(getDocRef(id));
-  const workflowDoc = await getDocRef(id);
+const asyncDeleteWorkflow = async (uid = '', id) => {
+  const workflowDoc = doc(db, 'users', uid, 'scripts', id);
   const workflowRef = await deleteDoc(workflowDoc);
   return workflowRef;
 };
