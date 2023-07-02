d31e78499e04909b3a5aea259092b380cce594bd
finnzzzz <o8786791@gmail.com>
2023-07-02 10:37:26 +0800

    refactor: remove unused type

--- web/src/app/api/workflowData/asyncSetWorkflow.ts
+++ web/src/app/api/workflowData/asyncSetWorkflow.ts
@@ -1,16 +1,6 @@
 import { setDoc, addDoc, doc, serverTimestamp, collection } from 'firebase/firestore';
 import { db } from '@/app/lib/firebase';
 
-type Flow = {
-  nodes: any[];
-  edges: any[];
-  viewport: {
-    x: number;
-    y: number;
-    zoom: number;
-  };
-};
-
 const asyncSetWorkflow = async (uid: string, { flow }: { flow: Flow }) => {
   const workflowDoc = collection(db, 'users', uid, 'scripts');
   const docRef = await addDoc(workflowDoc, {
