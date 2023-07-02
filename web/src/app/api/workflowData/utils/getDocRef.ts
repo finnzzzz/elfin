dc298d1a86b962262f1e9543b4deb1b0931272af
finnzzzz <o8786791@gmail.com>
2023-07-02 10:41:34 +0800

    feat: add types

--- web/src/app/api/workflowData/utils/getDocRef.ts
+++ web/src/app/api/workflowData/utils/getDocRef.ts
@@ -1,6 +1,6 @@
 import { doc } from 'firebase/firestore';
 import { db } from '@/app/lib/firebase';
 
-const getDocRef = (id) => doc(db, 'workflowtest', id);
+const getDocRef = (id:string) => doc(db, 'workflowtest', id);
 
 export default getDocRef;
