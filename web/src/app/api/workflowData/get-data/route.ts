8cf25144bb43e17ca2477637df033589f4cf411e
finnzzzz <o8786791@gmail.com>
2023-07-02 10:46:46 +0800

    refactor: ignore test code

--- web/src/app/api/workflowData/get-data/route.ts
+++ web/src/app/api/workflowData/get-data/route.ts
@@ -1,14 +1,17 @@
-import { NextResponse } from 'next/server';
-import { collection, getDocs } from 'firebase/firestore';
-import { db } from '@/app/lib/firebase';
+// import { NextResponse } from 'next/server';
+// import { collection, getDocs } from 'firebase/firestore';
+// import { db } from '@/app/lib/firebase';
 
-export async function GET() {
-  const allData = [];
-  const res = await getDocs(collection(db, 'workflowtest'));
-  res.forEach((doc) => {
-    allData.push(doc.data());
-  });
-  console.log(allData);
+// interface DocumentData {
 
-  return NextResponse.json(allData);
-}
+// }
+// export async function GET() {
+//   const allData = [];
+//   const res = await getDocs(collection(db, 'workflowtest'));
+//   res.forEach((doc) => {
+//     allData.push(doc.data());
+//   });
+//   console.log(allData);
+
+//   return NextResponse.json(allData);
+// }
