49ca32e68a614273239b185f3689937d54d8cd9c
finnzzzz <o8786791@gmail.com>
2023-07-05 10:35:20 +0800

    feat: add remote patterns for next image

--- web/next.config.js
+++ web/next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
