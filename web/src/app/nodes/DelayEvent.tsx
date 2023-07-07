b033491246cfabc8ed1908acdbe6702e0e24d52e
finnzzzz <o8786791@gmail.com>
2023-07-07 10:21:31 +0800

    feat: add contenxt menu and types

--- web/src/app/nodes/DelayEvent.tsx
+++ web/src/app/nodes/DelayEvent.tsx
@@ -1,55 +1,69 @@
-import React from 'react';
+import { memo } from 'react';
+
 import { Handle, Position } from 'reactflow';
 import useStore from '../store';
 import { shallow } from 'zustand/shallow';
+import ContextMenu from './utils/ContextMenu';
 
 type Event = React.ChangeEvent<HTMLInputElement>;
 
 const selector = (id: string) => (store: any) => ({
-  setDelayTime: (e: Event) => store.updateNode(id, { DelayTime: e.target.value }),
+  setDelayTime: (e: Event) => store.updateNode(id, { delayTime: e.target.value }),
 });
+
+type delayObj = {
+  label: 'Delay';
+  disable: boolean;
+  delayTime: string;
+};
 
 interface DelayEventProps {
   id: string;
   isConnectable: boolean;
+  data: delayObj;
 }
 
-const DelayEvent = ({ id, isConnectable }: DelayEventProps) => {
-  const nodes = useStore((state) => state.nodes);
-
+const DelayEvent = ({ id, isConnectable, data }: DelayEventProps) => {
   const { setDelayTime } = useStore(selector(id), shallow);
-  const DelayTime = nodes?.filter((item) => item.id == id)[0]?.data?.DelayTime;
 
   return (
-    <div className='h-[150px] w-[160px] rounded-md border border-blue-600 bg-white p-2'>
-      <Handle
-        type='target'
-        position={Position.Left}
-        isConnectable={isConnectable}
-        style={{ background: '#909cf9', width: '18px', height: '18px', left: '-24px' }}
-      />
-      <div className='text-2xl'>Delay</div>
-      <div className=' mb-4 text-xs'>{id}</div>
-
-      <div>
-        <label htmlFor='text'>Time：</label>
-        <input
-          id='text'
-          name='text'
-          onChange={setDelayTime}
-          value={DelayTime}
-          className='nodrag w-full rounded-sm border border-blue-300'
-        />
-      </div>
-      <Handle
-        type='source'
-        position={Position.Right}
-        id='delay'
-        isConnectable={isConnectable}
-        style={{ background: '#909cf9', width: '18px', height: '18px', right: '-24px' }}
-      />
-    </div>
+    <>
+      <ContextMenu id={id}>
+        <div
+          className={`h-[150px] w-[160px] rounded-md border border-blue-600 bg-white ${
+            data.disable ? 'toggleOpacity' : ''
+          } p-2`}
+        >
+          <Handle
+            type='target'
+            position={Position.Left}
+            isConnectable={isConnectable}
+            style={{ background: '#909cf9', width: '18px', height: '18px', left: '-24px' }}
+          />
+          <div className='text-2xl'>{data.label}</div>
+          <div className=' mb-4 text-xs'>{id}</div>
+          <div>
+            <label htmlFor='text'>Time：</label>
+            <input
+              id='text'
+              name='text'
+              onChange={setDelayTime}
+              value={data.delayTime}
+              placeholder='2000ms'
+              className='nodrag w-full rounded-sm border border-blue-300'
+            />
+          </div>
+          <Handle
+            type='source'
+            position={Position.Right}
+            id='delay'
+            isConnectable={isConnectable}
+            style={{ background: '#909cf9', width: '18px', height: '18px', right: '-24px' }}
+          />
+        </div>
+      </ContextMenu>
+    </>
   );
 };
 
-export default DelayEvent;
+export default memo(DelayEvent);
--- web/src/app/nodes/GetContentEvent.tsx
+++ web/src/app/nodes/GetContentEvent.tsx
@@ -2,52 +2,73 @@ import React from 'react';
 import { Handle, Position } from 'reactflow';
 import useStore from '../store';
 import { shallow } from 'zustand/shallow';
+import ContextMenu from './utils/ContextMenu';
 
 type Event = React.ChangeEvent<HTMLInputElement>;
 
 const selector = (id: string) => (store: any) => ({
   setXPath: (e: Event) => store.updateNode(id, { XPath: e.target.value }),
+  setDescription: (e: Event) => store.updateNode(id, { description: e.target.value }),
 });
+
+type getContentObj = {
+  label: 'GetContent';
+  disable: boolean;
+  XPath: string;
+  description: string;
+};
 
 interface DelayEventProps {
   id: string;
   isConnectable: boolean;
+  data: getContentObj;
 }
 
-const GetContentEvent = ({ id, isConnectable }: DelayEventProps) => {
-  const nodes = useStore((state) => state.nodes);
-
-  const { setXPath } = useStore(selector(id), shallow);
-  const XPath = nodes?.filter((item) => item.id == id)[0]?.data?.XPath;
+const GetContentEvent = ({ id, isConnectable, data }: DelayEventProps) => {
+  const { setXPath, setDescription } = useStore(selector(id), shallow);
 
   return (
-    <div className='h-[110px] rounded-md border border-blue-600 bg-white p-2'>
-      <Handle
-        type='target'
-        position={Position.Left}
-        isConnectable={isConnectable}
-        style={{ background: '#909cf9', width: '18px', height: '18px', left: '-24px' }}
-      />
-      <div className='text-2xl'>GetContent</div>
-      <div className=' mb-4 text-xs'>{id}</div>
-      <div>
-        <label htmlFor='text'>XPath：</label>
-        <input
-          id='text'
-          name='text'
-          onChange={setXPath}
-          value={XPath}
-          className='nodrag rounded-sm border border-blue-300'
+    <ContextMenu id={id}>
+      <div
+        className={`h-[190px] rounded-md border border-blue-600 bg-white p-2 ${
+          data.disable ? 'toggleOpacity' : ''
+        }`}
+      >
+        <Handle
+          type='target'
+          position={Position.Left}
+          isConnectable={isConnectable}
+          style={{ background: '#909cf9', width: '18px', height: '18px', left: '-24px' }}
+        />
+        <div className='text-2xl'>{data.label}</div>
+        <div className=' mb-4 text-xs'>{id}</div>
+        <div className=' flex flex-col'>
+          <label htmlFor='text'>XPath：</label>
+          <input
+            id='text'
+            name='text'
+            onChange={setXPath}
+            value={data.XPath}
+            className='nodrag rounded-sm border border-blue-300'
+          />
+          <label htmlFor='text'>Description：</label>
+          <input
+            id='text'
+            name='text'
+            onChange={setDescription}
+            value={data.description}
+            className='nodrag rounded-sm border border-blue-300'
+          />
+        </div>
+        <Handle
+          type='source'
+          position={Position.Right}
+          id='getContent'
+          isConnectable={isConnectable}
+          style={{ background: '#909cf9', width: '18px', height: '18px', right: '-24px' }}
         />
       </div>
-      <Handle
-        type='source'
-        position={Position.Right}
-        id='getContent'
-        isConnectable={isConnectable}
-        style={{ background: '#909cf9', width: '18px', height: '18px', right: '-24px' }}
-      />
-    </div>
+    </ContextMenu>
   );
 };
 
