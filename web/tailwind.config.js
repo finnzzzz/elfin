a52d788113c478cdb883a635d5b20b3f2b816234
finnzzzz <o8786791@gmail.com>
2023-07-09 15:10:31 +0800

    feat: set custom colors

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -6,7 +6,61 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainBlue: {
+          50: '#F0F6FF',
+          100: '#dae4ff',
+          200: '#bdd0ff',
+          300: '#90b2ff',
+          400: '#5383ff',
+          500: '#355ffc',
+        },
+        customGreen: {
+          50: '#F5FBE4',
+          400: '#2DA08C',
+          500: '#218171',
+        },
+        customOrange: {
+          50: '#FFF4EE',
+          400: '#fb8219',
+          500: '#d06909',
+        },
+        customPurple: {
+          50: '#F6F1FF',
+          400: '#7E51DD',
+          500: '#6b48b7',
+        },
+        customDarkYellow: {
+          50: '#FBFAE3',
+          400: '#f0b728',
+          500: '#cd9a18',
+        },
+        customPink: {
+          50: '#F8F3FF',
+          400: '#f890d5',
+          500: '#e277be',
+        },
+        customRed: {
+          50: '#FFF2EB',
+          400: '#F17070',
+          500: '#DA6060',
+        },
+        customNileBlue: {
+          50: '#ECFAFF',
+          400: '#27A5CD',
+          500: '#167FA0',
+        },
+        customDarkGreen: {
+          50: '#FFFEEC',
+          400: '#0F740D',
+          500: '#0B5F09',
+        },
+      },
+      borderRadius: {
+        nodebase: '16px',
+      },
+    },
   },
   plugins: [],
 };
