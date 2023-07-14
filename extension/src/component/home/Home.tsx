aa44560ba32ea8fe52bc911661fa61778e100c72
finnzzzz <o8786791@gmail.com>
2023-07-14 16:05:28 +0800

    feat: add log out function

--- extension/src/component/home/Home.tsx
+++ extension/src/component/home/Home.tsx
@@ -2,6 +2,7 @@ import List from './List';
 import { useState } from 'react';
 
 import { PiHandTap } from 'react-icons/pi';
+import { TbLogout } from 'react-icons/tb';
 
 const Home = () => {
   const [userToken, setUserToken] = useState(localStorage.getItem('extensionKey') || '');
@@ -42,6 +43,11 @@ const Home = () => {
     }
   });
 
+  const logout = () => {
+    localStorage.removeItem('extensionKey');
+    setUserToken('');
+  };
+
   return (
     <div className=' flex h-full justify-end'>
       <div className=' flex w-[330px] flex-col items-center pb-3 pt-6 font-semibold background-image-blue relative'>
@@ -53,19 +59,29 @@ const Home = () => {
         </div>
 
         {userToken ? (
-          <List key={userToken} />
+          <>
+            <button
+              onClick={logout}
+              className='text-gray-400 absolute top-[15px] right-[15px] hover:text-gray-600'
+            >
+              <TbLogout size='24px' />
+            </button>
+            <List />
+          </>
         ) : (
           <>
             <span className='mb-4'>
               <PiHandTap size='30px' />
             </span>
-            <span className=' text-base mb-1 '>Please log in on the elfin page first</span>
-            <span className=' text-xs mb-3 '>
+            <span className=' text-base mb-1 text-gray-700'>
+              Please log in on the elfin page first
+            </span>
+            <span className=' text-xs mb-3 text-gray-700'>
               connect the extension under the window of the website
             </span>
             <button
               onClick={userlogin}
-              className=' border border-blue-400 p-2 mb-3 rounded-md bg-white'
+              className=' border border-gray-400 hover:border-mainBlue-400 p-2 mb-3 rounded-md bg-white'
             >
               connect
             </button>
