277a288112e51532879283cec0c45c476485019e
finnzzzz <o8786791@gmail.com>
2023-07-18 09:42:26 +0800

    style: adjust landing page style

--- web/src/app/components/landingPage/LandingPage.tsx
+++ web/src/app/components/landingPage/LandingPage.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Image from 'next/image';
 import mainPic from './mainPicture.png';
+import chromeIcon from './chromeIcon.png';
 
 import Login from '../login/Login';
 
@@ -15,9 +16,9 @@ const LandingPage = () => {
     <>
       <div className=' fixed left-0 top-0 h-full w-full bg-white'>
         <Login togglePop={togglePop} seen={seen} />
-        <div className='flex h-full w-full  flex-col items-center p-5'>
+        <div className='flex h-full w-full  flex-col items-center p-7'>
           <span className=' self-end text-xl'>browser automation tool</span>
-          <div className=' mt-[10.5%] flex items-center gap-2 text-5xl font-bold'>
+          <div className=' mt-24 flex items-center gap-2 text-5xl font-bold 2xl:mt-48'>
             <span>make</span>
             <span className=' title-browser-bg rounded-[10px]  p-2 pl-3 pr-3 text-mainBlue-500'>
               browser
@@ -27,19 +28,30 @@ const LandingPage = () => {
               simple
             </span>
           </div>
-          <div className=' mt-[6%]'>
+          <div className=' mt-12 2xl:mt-20'>
             <Image
               src={mainPic}
               width={1236}
               height={316}
-              quality={87}
+              quality={90}
               className=' w-[1050px]'
               alt='landing page picture'
             />
           </div>
-          <button className=' mt-[150px] text-xl hover:underline' onClick={togglePop}>
+          <button className=' 2xl:mt-30 mt-20 text-xl hover:underline' onClick={togglePop}>
             sign in to start
           </button>
+          <div className=' mr-5 mt-[5.5%] flex items-center self-end rounded-full border border-black p-3 text-base'>
+            <Image
+              src={chromeIcon}
+              width={41}
+              height={40}
+              quality={90}
+              className=' mr-2 w-[30px]'
+              alt='download for chrome'
+            />
+            Download for Chrome
+          </div>
         </div>
       </div>
     </>
