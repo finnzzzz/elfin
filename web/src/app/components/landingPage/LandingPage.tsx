0e653ff5d423c6bf0c25360bdbf170259a097c09
finnzzzz <o8786791@gmail.com>
2023-07-23 18:56:56 +0800

    refactor: prettier

--- web/src/app/components/landingPage/LandingPage.tsx
+++ web/src/app/components/landingPage/LandingPage.tsx
@@ -41,7 +41,7 @@ const LandingPage = () => {
           <button className=' 2xl:mt-30 mt-20 text-xl hover:underline' onClick={togglePop}>
             sign in to start
           </button>
-          <div className=' mr-5 mt-[5.5%] flex items-center self-end rounded-full border border-black p-3 text-base cursor-pointer'>
+          <div className=' mr-5 mt-[5.5%] flex cursor-pointer items-center self-end rounded-full border border-black p-3 text-base'>
             <Image
               src={chromeIcon}
               width={41}
